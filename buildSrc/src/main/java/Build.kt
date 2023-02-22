21379bccb12ec8529be51c1b064cedce3e7b63ce
daniatitienei <daniatitienei@gmail.com>
2023-02-22 20:41:11 +0200

    migrated to kts

---
+++ buildSrc/src/main/java/Build.kt
@@ -0,0 +1,11 @@
+object Build {
+    const val androidVersion = "8.1.0-alpha06"
+
+    const val androidApplication = "com.android.application"
+    const val androidLibrary = "com.android.library"
+
+    const val kotlin = "org.jetbrains.kotlin.android"
+
+    private const val hiltAndroidGradlePluginVersion = "2.44"
+    const val hiltAndroidGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$hiltAndroidGradlePluginVersion"
+}
---
+++ buildSrc/src/main/java/ProjectConfig.kt
@@ -0,0 +1,8 @@
+object ProjectConfig {
+    const val appId = "com.atitienei_daniel.jetcalories"
+    const val compileSdk = 33
+    const val minSdk = 29
+    const val targetSdk = 31
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
