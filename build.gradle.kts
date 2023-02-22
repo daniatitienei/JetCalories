d7efdf9c67159f698c1e1a7b3740943cbdfc000f
daniatitienei <daniatitienei@gmail.com>
2023-02-22 21:37:41 +0200

    fixing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 buildscript {
     dependencies {
         classpath(Build.hiltAndroidGradlePlugin)
+        classpath(Build.androidTools)
     }
 }
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
--- buildSrc/src/main/java/Build.kt
+++ buildSrc/src/main/java/Build.kt
@@ -6,6 +6,8 @@ object Build {
 
     const val kotlin = "org.jetbrains.kotlin.android"
 
+    const val androidTools = "com.android.tools.build:gradle:7.2.0"
+
     private const val hiltAndroidGradlePluginVersion = "2.44"
     const val hiltAndroidGradlePlugin = "com.google.dagger:hilt-android-gradle-plugin:$hiltAndroidGradlePluginVersion"
 }
