9dac36d6265d4088882335addb30e88dbf464b30
daniatitienei <daniatitienei@gmail.com>
2023-02-26 00:52:47 +0200

    day selector

--- tracker/tracker_presentation/src/main/java/com/atitienei_daniel/tracker_presentation/overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/atitienei_daniel/tracker_presentation/overview/TrackerOverviewScreen.kt
@@ -3,7 +3,11 @@
 package com.atitienei_daniel.tracker_presentation.overview
 
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
@@ -16,6 +20,7 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.atitienei_daniel.core.util.UiEvent
 import com.atitienei_daniel.core_ui.LocalSpacing
+import com.atitienei_daniel.tracker_presentation.overview.components.DaySelector
 import com.atitienei_daniel.tracker_presentation.overview.components.NutrientsHeader
 
 @Composable
@@ -36,6 +41,21 @@ fun TrackerOverviewScreen(
         ) {
             item {
                 NutrientsHeader(state = uiState)
+                Spacer(modifier = Modifier.height(spacing.spaceMedium))
+
+                DaySelector(
+                    date = uiState.date,
+                    onPreviousDayClick = {
+                        viewModel.onEvent(TrackerOverviewEvent.OnPreviousDayClick)
+                    },
+                    onNextDayClick = {
+                        viewModel.onEvent(TrackerOverviewEvent.OnNextDayClick)
+                    },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = spacing.spaceMedium)
+                )
+                Spacer(modifier = Modifier.height(spacing.spaceMedium))
             }
         }
     }
