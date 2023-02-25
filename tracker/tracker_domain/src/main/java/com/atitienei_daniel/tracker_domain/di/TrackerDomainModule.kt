1917d3ab562ab1806b563b35c9038f2ad68b2437
daniatitienei <daniatitienei@gmail.com>
2023-02-25 09:11:10 +0200

    tracker use cases

---
+++ tracker/tracker_domain/src/main/java/com/atitienei_daniel/tracker_domain/di/TrackerDomainModule.kt
@@ -0,0 +1,34 @@
+package com.atitienei_daniel.tracker_domain.di
+
+import com.atitienei_daniel.core.domain.data_store.UserDataStore
+import com.atitienei_daniel.tracker_domain.repository.TrackerRepository
+import com.atitienei_daniel.tracker_domain.use_case.CalculateMealNutrients
+import com.atitienei_daniel.tracker_domain.use_case.DeleteTrackedFood
+import com.atitienei_daniel.tracker_domain.use_case.GetFoodsForDate
+import com.atitienei_daniel.tracker_domain.use_case.SearchFood
+import com.atitienei_daniel.tracker_domain.use_case.TrackFood
+import com.atitienei_daniel.tracker_domain.use_case.TrackerUseCases
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.scopes.ViewModelScoped
+
+@Module
+@InstallIn(ViewModelComponent::class)
+object TrackerDomainModule {
+
+    @ViewModelScoped
+    @Provides
+    fun providesTrackerUseCases(
+        repository: TrackerRepository,
+        dataStore: UserDataStore
+    ): TrackerUseCases =
+        TrackerUseCases(
+            searchFood = SearchFood(repository),
+            deleteTrackedFood = DeleteTrackedFood(repository),
+            trackFood = TrackFood(repository),
+            getFoodsForDate = GetFoodsForDate(repository),
+            calculateMealNutrients = CalculateMealNutrients(dataStore)
+        )
+}
