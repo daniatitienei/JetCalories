2e84e50c4145a87fc38ac5084599d2a6c9aa8821
daniatitienei <daniatitienei@gmail.com>
2023-02-26 13:16:06 +0200

    added tracked food item ui

--- tracker/tracker_presentation/src/main/java/com/atitienei_daniel/tracker_presentation/overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/atitienei_daniel/tracker_presentation/overview/TrackerOverviewScreen.kt
@@ -2,15 +2,26 @@
 
 package com.atitienei_daniel.tracker_presentation.overview
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -23,6 +34,7 @@ import com.atitienei_daniel.core_ui.LocalSpacing
 import com.atitienei_daniel.tracker_presentation.overview.components.DaySelector
 import com.atitienei_daniel.tracker_presentation.overview.components.ExpandableMeal
 import com.atitienei_daniel.tracker_presentation.overview.components.NutrientsHeader
+import com.atitienei_daniel.tracker_presentation.overview.components.TrackedFoodItem
 
 @Composable
 fun TrackerOverviewScreen(
@@ -64,9 +76,41 @@ fun TrackerOverviewScreen(
                     onToggleClick = {
                         viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal))
                     },
-                    modifier = Modifier.fillMaxWidth().padding(horizontal = spacing.spaceMedium)
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(horizontal = spacing.spaceMedium)
                 ) {
-
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+                        uiState.trackedFoods.forEach { trackedFood ->
+                            TrackedFoodItem(
+                                food = trackedFood,
+                                onDeleteClick = {
+                                    viewModel.onEvent(
+                                        TrackerOverviewEvent.OnDeleteTrackedFoodClick(trackedFood)
+                                    )
+                                }
+                            )
+                            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                        }
+                        OutlinedButton(
+                            onClick = {
+                                viewModel.onEvent(TrackerOverviewEvent.OnAddFoodClick(meal))
+                            },
+                            border = BorderStroke(
+                                width = 1.dp,
+                                color = MaterialTheme.colorScheme.primary
+                            ),
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            Icon(imageVector = Icons.Rounded.Add, contentDescription = null)
+                            Spacer(modifier = Modifier.width(spacing.spaceSmall))
+                            Text(text = "Add ${meal.name}")
+                        }
+                    }
                 }
             }
         }
